8b3a3109ad8670ddebc8c8b5e57b324e5f663f9b
Yahia El Tai <yahiaeltai022@gmail.com>
2023-04-22 16:47:44 +0200

    provide a different model

--- models/doctorsAppointments.js
+++ models/doctorsAppointments.js
@@ -6,7 +6,8 @@ Maybe before moving forward, you can spend some time and see if this could be si
 to avoid repeating code on the backend and as well on the frontend
 
 
-Here's a schema model that could potentially work
+Here's an alternative approach that separates the doctor's availability and booked appointments into separate models. 
+This will help you manage and query availability and appointments more efficiently.
 
 const doctorSchema = new mongoose.Schema({
   name: {
@@ -26,98 +27,241 @@ const doctorSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  weeklyAvailability: [
-    {
-      dayOfWeek: {
-        type: String,
-        enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-        required: true,
-      },
-      availableHours: [
-        {
-          startTime: {
-            type: String,
-            required: true,
-          },
-          endTime: {
-            type: String,
-            required: true,
-          },
-        },
-      ],
-      bookedSlots: [
-        {
-          date: {
-            type: Date,
-            required: true,
-          },
-          startTime: {
-            type: String,
-            required: true,
-          },
-          endTime: {
-            type: String,
-            required: true,
-          },
-          patientId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Patient',
-          },
-        },
-      ],
-    },
-  ],
 });
 
----------------------
-And here's how the doctor object could look like
+const availabilitySchema = new mongoose.Schema({
+  doctorId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Doctor',
+    required: true,
+  },
+  dayOfWeek: {
+    type: String,
+    enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    required: true,
+  },
+  startTime: {
+    type: String,
+    required: true,
+  },
+  endTime: {
+    type: String,
+    required: true,
+  },
+});
 
+const appointmentSchema = new mongoose.Schema({
+  doctorId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Doctor',
+    required: true,
+  },
+  patientId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Patient',
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  startTime: {
+    type: String,
+    required: true,
+  },
+  endTime: {
+    type: String,
+    required: true,
+  },
+});
+
+With this structure, you can manage doctors, their availabilities, and appointments independently. 
+This design will make it easier to query and manage appointments and availability. 
+For example, you can retrieve a doctor's availability without loading their appointment data or vice versa. 
+This separation of concerns will also make your application more maintainable and scalable.
+
+Here are example objects for the Doctor, Availability, and Appointment models
+
+---------- Doctor --------
 {
+  "_id": "60a7b9f5d5a5b9001555da21",
   "name": "Dr. Jane Doe",
   "email": "jane.doe@example.com",
   "phone": "555-1234",
-  "specialization": "Veterinarian",
-  "weeklyAvailability": [
-    {
-      "dayOfWeek": "Monday",
-      "availableHours": [
-        {
-          "startTime": "09:00",
-          "endTime": "12:00"
-        },
-        {
-          "startTime": "14:00",
-          "endTime": "18:00"
-        }
-      ],
-      "bookedSlots": [
-        {
-          "date": "2023-04-24",
-          "startTime": "09:30",
-          "endTime": "10:00",
-          "patientId": "607c1ea07e8466001570e7d9"
-        }
-      ]
-    },
-    {
-      "dayOfWeek": "Tuesday",
-      "availableHours": [
-        {
-          "startTime": "09:00",
-          "endTime": "12:00"
-        },
-        {
-          "startTime": "14:00",
-          "endTime": "18:00"
-        }
-      ],
-      "bookedSlots": []
-    }
-  ]
+  "specialization": "Veterinarian"
 }
 
+---------- Availability -----------
+[
+  {
+    "_id": "60a7bb2dd5a5b9001555da22",
+    "doctorId": "60a7b9f5d5a5b9001555da21",
+    "dayOfWeek": "Monday",
+    "startTime": "09:00",
+    "endTime": "12:00"
+  },
+  {
+    "_id": "60a7bb2dd5a5b9001555da23",
+    "doctorId": "60a7b9f5d5a5b9001555da21",
+    "dayOfWeek": "Monday",
+    "startTime": "14:00",
+    "endTime": "18:00"
+  },
+  {
+    "_id": "60a7bb2dd5a5b9001555da24",
+    "doctorId": "60a7b9f5d5a5b9001555da21",
+    "dayOfWeek": "Tuesday",
+    "startTime": "09:00",
+    "endTime": "12:00"
+  },
+  {
+    "_id": "60a7bb2dd5a5b9001555da25",
+    "doctorId": "60a7b9f5d5a5b9001555da21",
+    "dayOfWeek": "Tuesday",
+    "startTime": "14:00",
+    "endTime": "18:00"
+  }
+]
 
+----------- Appointment ------------
+[
+  {
+    "_id": "60a7bd5ad5a5b9001555da26",
+    "doctorId": "60a7b9f5d5a5b9001555da21",
+    "patientId": "607c1ea07e8466001570e7d9",
+    "date": "2023-04-24",
+    "startTime": "09:30",
+    "endTime": "10:00"
+  },
+  {
+    "_id": "60a7bd5ad5a5b9001555da27",
+    "doctorId": "60a7b9f5d5a5b9001555da21",
+    "patientId": "607c1ea07e8466001570e7da",
+    "date": "2023-04-24",
+    "startTime": "10:00",
+    "endTime": "10:30"
+  }
+]
+
+These example objects demonstrate how the data is structured with the separated schemas. 
+The doctorId field in both the Availability and Appointment models is used to reference the related Doctor object. 
+The patientId field in the Appointment model can be used to reference a separate Patient schema or any other schema you have for patients.
+
+------- Common queries to help you visualize it ------------
+
+1. Get a doctor's availability:
+const doctorId = '60a7b9f5d5a5b9001555da21';
+const availability = await Availability.find({ doctorId });
+
+2. Get a doctor's availability for a specific day of the week:
+const doctorId = '60a7b9f5d5a5b9001555da21';
+const dayOfWeek = 'Monday';
+const availability = await Availability.find({ doctorId, dayOfWeek });
+
+
+3. Get a doctor's appointments:
+const doctorId = '60a7b9f5d5a5b9001555da21';
+const appointments = await Appointment.find({ doctorId }).populate('patientId');
+
+
+4. Get a doctor's appointments for a specific date:
+const doctorId = '60a7b9f5d5a5b9001555da21';
+const date = new Date('2023-04-24');
+const appointments = await Appointment.find({
+  doctorId,
+  date,
+}).populate('patientId');
+
+
+5. Get available time slots for a specific doctor on a specific date:
+const doctorId = '60a7b9f5d5a5b9001555da21';
+const date = new Date('2023-04-24');
+const dayOfWeek = date.toLocaleString('en-US', { weekday: 'long' });
+
+const availability = await Availability.find({ doctorId, dayOfWeek });
+const appointments = await Appointment.find({ doctorId, date });
+
+// Function to calculate available time slots based on existing appointments
+const getAvailableTimeSlots = (availability, appointments) => {
+  // Your logic here
+};
+
+const availableTimeSlots = getAvailableTimeSlots(availability, appointments);
+
+
+6. Get all appointments for a specific patient:
+const patientId = '607c1ea07e8466001570e7d9';
+const appointments = await Appointment.find({ patientId }).populate('doctorId');
+
+
+7. Get appointments for a specific doctor within a date range:
+const doctorId = '60a7b9f5d5a5b9001555da21';
+const startDate = new Date('2023-04-01');
+const endDate = new Date('2023-04-30');
+const appointments = await Appointment.find({
+  doctorId,
+  date: { $gte: startDate, $lte: endDate },
+}).populate('patientId');
+
+
+8. Create a new appointment for a specific doctor on a specific day with a specific time:
+  1. First, validate if the requested time slot is available for the doctor:
+    const doctorId = '60a7b9f5d5a5b9001555da21';
+    const patientId = '607c1ea07e8466001570e7d9';
+    const appointmentDate = new Date('2023-04-24');
+    const appointmentStartTime = '09:30';
+    const appointmentEndTime = '10:00';
+    const dayOfWeek = appointmentDate.toLocaleString('en-US', { weekday: 'long' });
+
+    const availability = await Availability.findOne({
+    doctorId,
+    dayOfWeek,
+    startTime: { $lte: appointmentStartTime },
+    endTime: { $gte: appointmentEndTime },
+    });
+
+    if (!availability) {
+    // Handle the case when the doctor is not available during the requested time slot
+    }
+
+  2. Then, check if there are no overlapping appointments for the doctor:
+
+    const overlappingAppointment = await Appointment.findOne({
+      doctorId,
+      date: appointmentDate,
+      $or: [
+        {
+          startTime: { $lt: appointmentEndTime },
+          endTime: { $gt: appointmentStartTime },
+        },
+        {
+          startTime: { $lte: appointmentStartTime },
+          endTime: { $gte: appointmentEndTime },
+        },
+        {
+          startTime: { $gte: appointmentStartTime },
+          endTime: { $lte: appointmentEndTime },
+        },
+      ],
+    });
+
+    if (overlappingAppointment) {
+      // Handle the case when there is an overlapping appointment
+    }
+
+  3. If the requested time slot is available and there are no overlapping appointments, create a new appointment:
+
+    const newAppointment = new Appointment({
+      doctorId,
+      patientId,
+      date: appointmentDate,
+      startTime: appointmentStartTime,
+      endTime: appointmentEndTime,
+    });
+
+    await newAppointment.save();
 */
+
 const doctorsAppointmentsSchema = new mongoose.Schema(
     {
         doctor: {
